68a90eae46d0e1a21e51a8838d59063a98d75e18
Sonya Parau <sonyaparau@yahoo.com>
2021-03-20 20:45:01 +0200

    OpenAPI in the login controller

--- src/main/java/com/jobportal/jobportal/controller/HelloController.java
+++ src/main/java/com/jobportal/jobportal/controller/HelloController.java
@@ -1,24 +1,25 @@
 package com.jobportal.jobportal.controller;
 
-import com.jobportal.openapi.api.HelloApi;
-import com.jobportal.openapi.model.Hello;
+//import com.jobportal.openapi.api.HelloApi;
+//import com.jobportal.openapi.model.Hello;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class HelloController implements HelloApi {
-    @Override
-    public ResponseEntity<List<Hello>> helloGet() {
-        Hello hello1 = new Hello();
-        hello1.setId(100L);
-        hello1.setMessage("Hello message 1!");
-
-        Hello hello2 = new Hello();
-        hello2.setId(200L);
-        hello2.setMessage("Hello message 2");
-
-        return ResponseEntity.ok(List.of(hello1, hello2));
-    }
+public class HelloController  {
+    //TODO Uncomment this if you want to see the OPEN API example
+//    @Override
+//    public ResponseEntity<List<Hello>> helloGet() {
+//        Hello hello1 = new Hello();
+//        hello1.setId(100L);
+//        hello1.setMessage("Hello message 1!");
+//
+//        Hello hello2 = new Hello();
+//        hello2.setId(200L);
+//        hello2.setMessage("Hello message 2");
+//
+//        return ResponseEntity.ok(List.of(hello1, hello2));
+//    }
 }
