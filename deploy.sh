8af3ad6e3737fe183c46c59b22fbf4c95c0b9a76
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-04-03 16:56:51 +0300

    cd: added logs to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo "Reverting modified files (mvnw, pom.xml)..."
 git restore mvnw pom.xml
+echo "Reverting files successful. Installing heroku CLI..."
 curl https://cli-assets.heroku.com/install.sh | sh;  #install heroku
+echo "Heroku CLI installed, logging into heroku registry..."
 docker login --username=$HEROKU_LOGIN --password=$HEROKU_API_KEY registry.heroku.com;  #login to registry.heroku.com
+echo "Login successful, pushing container to heroku container registry..."
 docker push registry.heroku.com/$HEROKU_APP/web;
+echo "Push successful, deploying..."
 heroku container:release web --app $HEROKU_APP
+echo "Deployment successful. Tagging release"
 git tag -a v0.0.1 -m "Release v0.0.1"
 git push
