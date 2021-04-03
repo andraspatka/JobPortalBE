b67c896776c3c10cb6c347f70bf4cca958743374
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-04-03 19:37:22 +0300

    feat: added hello world endpoint

--- src/main/java/com/jobportal/jobportal/controller/HelloController.java
+++ src/main/java/com/jobportal/jobportal/controller/HelloController.java
@@ -1,25 +1,21 @@
 package com.jobportal.jobportal.controller;
 
-//import com.jobportal.openapi.api.HelloApi;
-//import com.jobportal.openapi.model.Hello;
+import com.jobportal.openapi.api.HelloApi;
+import com.jobportal.openapi.model.Hello;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Random;
 
 @RestController
-public class HelloController  {
-    //TODO Uncomment this if you want to see the OPEN API example
-//    @Override
-//    public ResponseEntity<List<Hello>> helloGet() {
-//        Hello hello1 = new Hello();
-//        hello1.setId(100L);
-//        hello1.setMessage("Hello message 1!");
-//
-//        Hello hello2 = new Hello();
-//        hello2.setId(200L);
-//        hello2.setMessage("Hello message 2");
-//
-//        return ResponseEntity.ok(List.of(hello1, hello2));
-//    }
+public class HelloController implements HelloApi {
+
+    @Override
+    public ResponseEntity<Hello> helloGet() {
+        Hello hello = new Hello();
+        hello.setId(new Random().nextLong());
+        hello.setMessage("Hello World!");
+
+        return ResponseEntity.ok(hello);
+    }
 }
