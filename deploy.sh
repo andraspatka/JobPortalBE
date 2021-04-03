fcf96d6b6e696850e72999fdd7a5c51040021a44
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-04-03 18:56:01 +0300

    cd: added heroku login info to jib, added back version as tag

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@ echo "Installing Heroku CLI"
 curl https://cli-assets.heroku.com/install.sh | sh
 echo "Heroku CLI installed"
 
+COMMITS_SINCE_MAIN=$(git rev-list --count HEAD)
+TAG_NAME="${TRAVIS_BRANCH}-m.${COMMITS_SINCE_MAIN}"
+VERSION_NAME="0.0.1-${TAG_NAME}"
+echo "Version ${VERSION_NAME}"
+./mvnw versions:set -DnewVersion="${VERSION_NAME}"
+
 # Build and push docker image
 echo "Building and pushing docker image with JIB"
 ./mvnw compile jib:build
@@ -11,5 +17,8 @@ echo "Deploying..."
 heroku container:release web --app $HEROKU_APP
 
 echo "Deployment successful. Tagging release"
-git tag -a v0.0.1 -m "Release v0.0.1"
+git status
+git revert pom.xml mvnw
+git checkout ${TRAVIS_BRANCH}
+git tag -a v0.0.1 -m "Release v${VERSION_NAME}"
 git push
